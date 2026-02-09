9c1196bf2328a69e645d89ed123e94b5863d30b6
akshi2424 <akshi6265@gmail.com>
2026-02-09 23:04:15 +0530

    Time: 2 ms (99.44%), Space: 45 MB (52.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n1=s.length();
+        int n2=t.length();
+        if(n1!=n2)return false;
+        int count[]=new int[26];
+        for(char ch:s.toCharArray()){
+            count[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            count[ch-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(count[i]!=0)return false;
+        }
+        return true;
+        
+    }
+}
